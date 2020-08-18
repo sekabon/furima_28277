b4cfe22aef9b28f9514ab505e804501c213e73c9
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-18 16:04:54 +0900

    エラーメッセージ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :move_to_index, except: :index
   protected
   
   def configure_permitted_parameters 
@@ -10,7 +11,9 @@ class ApplicationController < ActionController::Base
   
 
   private
-
+  #def move_to_index
+    #redirect_to action: :index unless user_signed_in?
+  #end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
