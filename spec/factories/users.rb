25ea8fbb36b502f0a557cb92d99daa7d3d3978f1
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-19 18:41:49 +0900

    テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname            {"fuji"}
-    email               {"fff@gmalil.com"}
-    password            {"000000"}
+    email               {'fff@gmail.com'}
+    password            {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name         { '藤田' }
     first_name          { '敏弥' }
     family_name_reading { 'フジタ' }
     first_name_reading  { 'トシヤ' }
-    birthday            {"00000000"}
+    birthday            {Faker::Date.between(from: '1930-01-01', to: '2010-12-31')}
   end
 end
