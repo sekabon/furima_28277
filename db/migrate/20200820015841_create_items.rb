2bfdd29d7ce4763655086c6f52065e7d29d4fb6d
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-20 11:19:53 +0900

    itemsテーブル作成

---
+++ db/migrate/20200820015841_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,          null: false
+      t.text   :introduction,  null: false
+      t.integer :category_id,  null: false
+      t.integer :condition_id, null: false
+      t.integer :postagge_type_id, null: false
+      t.integer :ship_days_id, null: false
+      t.integer :ship_area_id, null: false
+      t.integer :price,        null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_030059) do
+ActiveRecord::Schema.define(version: 2020_08_20_015841) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postagge_type_id", null: false
+    t.integer "ship_days_id", null: false
+    t.integer "ship_area_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
