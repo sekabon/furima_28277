723de1b77db67622a072281a4f06e2bcb4331f97
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-26 19:27:18 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -41,7 +40,9 @@ class ItemsController < ApplicationController
   end
 
   private
-
+  def set_item
+    @item = Item.find(params[:id])
+  end
   def item_params
     params.require(:item).permit(:content, :image, :name, :introduction, :category_id, :condition_id, :postage_type_id, :ship_area_id, :ship_days_id, :price).merge(user_id: current_user.id)
   end
