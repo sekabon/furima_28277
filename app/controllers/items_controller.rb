8fdd50192f94dbd781c105fef8dce5f533e0e29e
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-26 20:21:56 +0900

    空になったメソッドの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,20 +19,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
   def update
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
-  end
-
-  def destroy
   end
 
   def move_to_index
