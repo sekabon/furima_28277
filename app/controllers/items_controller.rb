2c5c17a0494e35ebdb30bd9cb7428478dc3c489c
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-26 11:32:32 +0900

    商品詳細画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
     @items = Item.includes(:user).order('created_at DESC')
   end
 
-  def new
+  def new 
     @item = Item.new
   end
 
@@ -15,6 +15,9 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+  def show
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
