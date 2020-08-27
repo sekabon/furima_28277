c4b4aeafb0928d2664abb4d89912f2f4881012eb
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-27 11:35:26 +0900

    エラーハンドリング作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    set_item.destroy
-    redirect_to root_path
+    if set_item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def update
