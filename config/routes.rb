be060519fb133bde469503d12439ee2e275e8c5c
sekabon <66765282+sekabon@users.noreply.github.com>
2020-08-27 16:24:08 +0900

    card.js仮設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items do
-    resources :purchases, only: :index
+    resources :purchases, only: [:index, :create]
   end
   
 end
