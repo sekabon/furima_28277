16bef4063a6481e71163bb17731f8ec79a1cf97d
sekabon <66765282+sekabon@users.noreply.github.com>
2020-09-03 23:30:01 +0900

    キーの設定

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -16,3 +16,4 @@ class ItemPurchase
     DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
+
