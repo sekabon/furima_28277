f90469d791052421184e36ff1ab696e5b5f660e4
sekabon <66765282+sekabon@users.noreply.github.com>
2020-09-04 08:25:38 +0900

    reキーの設定

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -16,4 +16,3 @@ class ItemPurchase
     DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
-
