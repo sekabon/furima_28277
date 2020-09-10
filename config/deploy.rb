d609b204034bda4a4461049b9306bb999f887e63
sekabon <66765282+sekabon@users.noreply.github.com>
2020-09-10 12:33:32 +0900

    unicorn編集2

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid for current version and patch releases of Capistrano
+lock "3.14.1"
+
+set :application, "furima_28277"
+set :repo_url, "git@github.com:sekabon/furima_28277.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/28277key.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
